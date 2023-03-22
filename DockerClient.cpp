e226bee4b30dd79bb288b84be89ec9925d28c8ec
RioFutaba <carlo@paeony.org>
2023-03-22 08:51:09 +0800

    Temporary storage

--- DockerClient.cpp
+++ DockerClient.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "DockerClient.h"
+#include <unistd.h>
+
+DockerClient::DockerClient(std::string host):unix_sock_addr(host) {
+
+
+}
+
+
+auto DockerClient::init() ->void {
+    if (fd != -1) {
+        return;
+    }
+    if (fd == -1){
+        fd = socket(AF_UNIX,SOCK_STREAM,0);
+    }
+    sockaddr_un address ;
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path,unix_sock_addr.c_str());
+    int len = sizeof(address);
+    if (connect(fd, (struct sockaddr *)&address, len) == -1){
+        connect_status =-1;
+        return;
+    }
+    connect_status = 1;
+}
+
+auto DockerClient::list_containers() ->std::string {
+    std::string  uri("GET /containers/json HTTP/1.1\\r\\nHost: localhost\\r\\nAccept: application/json\\r\\n\\r\\n");
+    if (connect_status !=1 ){
+        init();
+    }
+    if(write(fd,uri.c_str(),strlen(uri.c_str())) == -1){
+        return "";
+    }
+    char buffer[buffer_size];
+    int n = recv(fd,buffer,buffer_size,0);
+    return n > 0 ? std::string(buffer):"";
+}
--- DockerClient.h
+++ DockerClient.h
@@ -4,10 +4,30 @@
 
 #ifndef DOCKERCLIENT_DOCKERCLIENT_H
 #define DOCKERCLIENT_DOCKERCLIENT_H
-
-
+#include <string>
+#include <map>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <sys/socket.h>
 class DockerClient {
+public:
 
+    DockerClient(std::string host);
+
+
+
+    //containers
+    std::string list_containers();
+
+
+
+private:
+    void init();
+    std::map<std::string,std::string> container_list;
+    int fd = -1;
+    std::string unix_sock_addr;
+    int buffer_size=4*1024*1024;
+    int connect_status;
 };
 
 
